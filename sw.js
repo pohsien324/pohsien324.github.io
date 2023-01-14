cb9ab2d07445776bb686b75180dbdf60d34c0d07
pohsienshih <pohsien324@gmail.com>
2023-01-14 14:00:47 +0800

    Add New Post: 2022 Review

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220801.1606';
+const cacheName = 'chirpy-20230114.0606';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
